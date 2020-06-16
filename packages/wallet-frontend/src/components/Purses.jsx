ed88f9da27c8903124d994707c06b98d3933cc76
Kate Sills <kate@agoric.com>
2020-06-16 15:14:07 -0700

    works

--- packages/wallet-frontend/src/components/Purses.jsx
+++ packages/wallet-frontend/src/components/Purses.jsx
@@ -12,6 +12,20 @@ import PurseIcon from '@material-ui/icons/BusinessCenter';
 
 import { useApplicationContext } from '../contexts/Application';
 
+const noOp = () => {};
+const slotToDisplay = slot => slot;
+// const { unserialize: display } = makeMarshal(noOp, slotToDisplay);
+const display = () => {};
+
+const displayExtent = amount => {
+  const { brand, extent } = display(amount);
+  // TODO: this check should be in boardID terms
+  if (brand.petname === 'zoe invite') {
+    return `${extent} ${brand.petname}`;
+  }
+  return `${JSON.stringify(extent)} ${brand.petname}`;
+};
+
 const useStyles = makeStyles(theme => ({
   icon: {
     minWidth: 24,
@@ -30,21 +44,29 @@ export default function Purses() {
       {Array.isArray(purses) && purses.length > 0 ? (
         <List>
           {purses.map(
-            ({ pursePetname, brandRegKey, issuerPetname, extent, currentAmount }) => (
+            ({
+              pursePetname,
+              brandRegKey,
+              issuerPetname,
+              extent,
+              currentAmount,
+            }) => (
               <ListItem key={pursePetname} value={pursePetname} divider>
                 <ListItemIcon className={classes.icon}>
                   <PurseIcon />
                 </ListItemIcon>
                 <ListItemText
                   primary={pursePetname}
-                  secondary={
+                  secondary={(
                     <>
                       <b>
-                        {JSON.stringify(extent)} {issuerPetname}
-                      </b>{' '}
-                      
+                        {JSON.stringify(extent)} 
+{' '}
+{issuerPetname}
+                      </b>
+{' '}
                     </>
-                  }
+                  )}
                 />
               </ListItem>
             ),
